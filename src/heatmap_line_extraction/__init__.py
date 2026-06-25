0666f290f485c1335a6022ec0818d5d3ed67e7f1
AhmadAZ-R <ahmad.azizpour990@gmail.com>
2026-06-25 02:33:03 +0200

    Create __init__.py

---
+++ src/heatmap_line_extraction/__init__.py
@@ -0,0 +1,14 @@
+"""
+heatmap_line_extraction
+
+Extract quantitative data and line profiles
+from scientific heatmap images.
+"""
+
+from .extractor import HeatmapExtractor
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "HeatmapExtractor"
+]
